73d764f79172dba60348d8fb3ac43e737c2c5e24
puszkarek <guipuszkarek@gmail.com>
2022-11-20 15:34:11 -0300

    🧪 Fix spec

--- apps/user-demo/src/app/stores/root/store-testing.module.ts
+++ apps/user-demo/src/app/stores/root/store-testing.module.ts
@@ -1,7 +1,7 @@
 import { HttpClientTestingModule } from '@angular/common/http/testing';
 import { NgModule } from '@angular/core';
 import { EntityStoreModule } from '@front/stores/root';
-import { UsersStore } from '@front/stores/users';
+import { UsersStore } from '@front/stores/users/users.store';
 
 const stores = [UsersStore];
 

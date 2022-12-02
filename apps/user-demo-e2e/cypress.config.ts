bc7944b99267534bcd36a1986e6341e68403723f
puszkarek <guipuszkarek@gmail.com>
2022-12-02 18:55:12 -0300

    ♻️ Update cypress demo spec

--- apps/user-demo-e2e/cypress.config.ts
+++ apps/user-demo-e2e/cypress.config.ts
@@ -1,6 +1,13 @@
-import { defineConfig } from 'cypress';
 import { nxE2EPreset } from '@nrwl/cypress/plugins/cypress-preset';
+import { defineConfig } from 'cypress';
 
 export default defineConfig({
-  e2e: nxE2EPreset(__dirname),
+  e2e: {
+    // eslint-disable-next-line unicorn/prefer-module
+    ...nxE2EPreset(__dirname),
+    baseUrl: 'http://localhost:4200',
+  },
+
+  // * Paths
+  fixturesFolder: './src/fixtures',
 });
--- apps/user-demo-e2e/src/e2e/app.cy.ts
+++ apps/user-demo-e2e/src/e2e/app.cy.ts
@@ -3,11 +3,15 @@ import { getGreeting } from '../support/app.po';
 describe('user-demo', () => {
   beforeEach(() => cy.visit('/'));
 
-  it('should display welcome message', () => {
-    // Custom command example, see `../support/commands.ts` file
-    cy.login('my-email@something.com', 'myPassword');
+  it('should display a Login Form', () => {
+    /*     // Custom command example, see `../support/commands.ts` file
+    cy.login('my-email@something.com', 'myPassword'); */
 
     // Function helper example, see `../support/app.po.ts` file
-    getGreeting().contains('Welcome user-demo');
+    // Displays an Logo with the App Name
+    getGreeting().contains('Demo');
+
+    // Contains a `Login` title
+    cy.contains('Login');
   });
 });

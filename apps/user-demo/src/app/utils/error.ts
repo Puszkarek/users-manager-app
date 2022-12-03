9ac2741f6efce49b03d2c2486bc7f9db73dca5e5
puszkarek <guipuszkarek@gmail.com>
2022-12-03 00:27:25 -0300

    🔇 Remove debug log

--- apps/user-demo/src/app/utils/error.ts
+++ apps/user-demo/src/app/utils/error.ts
@@ -3,7 +3,6 @@ import { UNKNOWN_ERROR_MESSAGE } from '@front/app/constants/messages';
 import { isError, isString } from 'lodash-es';
 
 export const toError = (value: unknown): Error => {
-  console.log('value', value);
   if (value instanceof HttpErrorResponse) {
     const errorMessage: unknown = value.error?.message;
     return new Error(isString(errorMessage) ? errorMessage : UNKNOWN_ERROR_MESSAGE);

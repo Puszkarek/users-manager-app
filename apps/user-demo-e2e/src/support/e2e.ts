ade6f0aea33978c233ca5288ad6cd96e2da2779e
puszkarek <guipuszkarek@gmail.com>
2022-12-03 17:46:51 -0300

    🚨 Disable eslint for file

--- apps/user-demo-e2e/src/support/e2e.ts
+++ apps/user-demo-e2e/src/support/e2e.ts
@@ -1,12 +1,13 @@
+/* eslint-disable unicorn/prevent-abbreviations */
 // ***********************************************************
 // This example support/index.js is processed and
-// loaded automatically before your test files.
+// Loaded automatically before your test files.
 //
 // This is a great place to put global configuration and
-// behavior that modifies Cypress.
+// Behavior that modifies Cypress.
 //
 // You can change the location of this file or turn off
-// automatically serving support files with the
+// Automatically serving support files with the
 // 'supportFile' configuration option.
 //
 // You can read more here:

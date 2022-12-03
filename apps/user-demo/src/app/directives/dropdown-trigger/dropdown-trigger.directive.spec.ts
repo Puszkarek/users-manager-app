f81a3d431abcaae0235d4287e51d042ac7cc2604
puszkarek <guipuszkarek@gmail.com>
2022-12-03 00:27:09 -0300

    🧪 Update test

--- apps/user-demo/src/app/directives/dropdown-trigger/dropdown-trigger.directive.spec.ts
+++ apps/user-demo/src/app/directives/dropdown-trigger/dropdown-trigger.directive.spec.ts
@@ -1,8 +1,11 @@
 import { DropdownTriggerDirective } from './dropdown-trigger.directive';
 
 describe('DropdownTriggerDirective', () => {
+  let directive!: DropdownTriggerDirective;
+  beforeEach(() => {
+    directive = new DropdownTriggerDirective(null!, null!, null!);
+  });
   it('should create an instance', () => {
-    const directive = new DropdownTriggerDirective();
     expect(directive).toBeTruthy();
   });
 });

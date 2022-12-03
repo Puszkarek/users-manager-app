da9d4600abda1721d1c8201eb7a8172cceb876b9
puszkarek <guipuszkarek@gmail.com>
2022-12-03 15:04:49 -0300

    🧪 Update specs

--- apps/user-demo/src/app/components/nav-bar/nav-bar.component.spec.ts
+++ apps/user-demo/src/app/components/nav-bar/nav-bar.component.spec.ts
@@ -1,14 +1,18 @@
+import { HttpClientTestingModule } from '@angular/common/http/testing';
 import { ComponentFixture, TestBed } from '@angular/core/testing';
+import { RouterTestingModule } from '@angular/router/testing';
+import { StoreTestingModule } from '@front/app/stores/root/store-testing.module';
 
 import { NavBarComponent } from './nav-bar.component';
+import { NavBarModule } from './nav-bar.module';
 
-describe('NavBarComponent', () => {
+describe(NavBarComponent.name, () => {
   let component: NavBarComponent;
   let fixture: ComponentFixture<NavBarComponent>;
 
   beforeEach(async () => {
     await TestBed.configureTestingModule({
-      declarations: [NavBarComponent],
+      imports: [HttpClientTestingModule, RouterTestingModule, StoreTestingModule, NavBarModule],
     }).compileComponents();
 
     fixture = TestBed.createComponent(NavBarComponent);
--- apps/user-demo/src/app/pages/login-page/login-page.component.spec.ts
+++ apps/user-demo/src/app/pages/login-page/login-page.component.spec.ts
@@ -1,5 +1,5 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
-import { StoreTestingModule } from '@front/app/stores/root';
+import { StoreTestingModule } from '@front/app/stores/root/store-testing.module';
 
 import { LoginPageComponent } from './login-page.component';
 import { LoginPageModule } from './login-page.module';

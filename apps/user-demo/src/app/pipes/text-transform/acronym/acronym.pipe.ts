fd296ee34d5c42e83b168baf3da05a1aea8b8e02
puszkarek <guipuszkarek@gmail.com>
2022-12-04 01:55:46 -0300

    ♻️ Fix behavior for pipe

--- apps/user-demo/src/app/pipes/text-transform/acronym/acronym.pipe.ts
+++ apps/user-demo/src/app/pipes/text-transform/acronym/acronym.pipe.ts
@@ -5,6 +5,15 @@ import { Pipe, PipeTransform } from '@angular/core';
 })
 export class AcronymPipe implements PipeTransform {
   public transform(value: string): string {
-    return value.slice(0, 1);
+    return (
+      value
+        // Split compound name (e.g `Big Name` => `['Big','Name']`)
+        .split(' ')
+        // Get the acronym from string
+        .map(splittedValue => splittedValue.slice(0, 1))
+        // Join the array into a string again
+        .join('')
+        .toUpperCase()
+    );
   }
 }

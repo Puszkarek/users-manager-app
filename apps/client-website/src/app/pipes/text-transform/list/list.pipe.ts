2580a78b19f792aca5ad09e632c5b92d610ac11d
puszkarek <guipuszkarek@gmail.com>
2022-12-10 12:32:11 -0300

    📝 Add `docs` for `pipe`s

--- apps/client-website/src/app/pipes/text-transform/list/list.pipe.ts
+++ apps/client-website/src/app/pipes/text-transform/list/list.pipe.ts
@@ -5,6 +5,12 @@ import { capitalize, map } from 'lodash-es';
   name: 'list',
 })
 export class ListPipe implements PipeTransform {
+  /**
+   * Parse a array into a readable string (e.g: `['1', '2']` becomes `1, 2`)
+   *
+   * @param value - The array to parse
+   * @returns A list in text format
+   */
   public transform(value: ReadonlyArray<string>): string {
     return map(value, capitalize).join(', ');
   }

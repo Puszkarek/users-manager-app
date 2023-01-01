95797c7045eba37e818b1fe5d6075d11545f4a25
puszkarek <guipuszkarek@gmail.com>
2023-01-01 20:04:21 -0300

    ✨ Create a lib for `global` utils

---
+++ libs/utils/src/lib/validations.ts
@@ -0,0 +1,9 @@
+/** Utils to handle functional pattern in a easy way */
+
+/**
+ * A little abstraction to use on pipes
+ *
+ * .eg: obs$.pipe(map(...), filter(isTrue))
+ */
+export const isTrue = (value: boolean): boolean => value;
+export const isFalse = (value: boolean): boolean => !value;
